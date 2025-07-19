a392c9f3f3288f973bfd97e40c39ebdada9a7653
Aitor <agarcb20@estudiantes.unileon.es>
2025-07-20 01:00:33 +0200

    Cambios ruta yolo models config.py

--- 04.Codigo/lib/YOLO_lib/config.py
+++ 04.Codigo/lib/YOLO_lib/config.py
@@ -7,17 +7,17 @@ IMGSZ = 704
 
 models = {
     # YOLOv11
-    "yolov11n": "yolo_models/yolo11n.pt",
-    "yolov11s": "yolo_models/yolo11s.pt",
-    "yolov11m": "yolo_models/yolo11m.pt",
-    "yolov11l": "yolo_models/yolo11l.pt",
-    "yolov11x": "yolo_models/yolo11x.pt",
-    "yolov11ny": "yolo_models/yolo11n.yaml",
+    "yolov11n": "yolo_output/yolo_models/yolo11n.pt",
+    "yolov11s": "yolo_output/yolo_models/yolo11s.pt",
+    "yolov11m": "yolo_output/yolo_models/yolo11m.pt",
+    "yolov11l": "yolo_output/yolo_models/yolo11l.pt",
+    "yolov11x": "yolo_output/yolo_models/yolo11x.pt",
+    "yolov11ny": "yolo_output/yolo_models/yolo11n.yaml",
 
     #YOLOv12
-    "yolov12n": "yolo_models/yolo12n.pt",
-    "yolov12s": "yolo_models/yolo12s.pt",
-    "yolov12m": "yolo_models/yolo12m.pt",
-    "yolov12l": "yolo_models/yolo12l.pt",
-    "yolov12x": "yolo_models/yolo",
+    "yolov12n": "yolo_output/yolo_models/yolo12n.pt",
+    "yolov12s": "yolo_output/yolo_models/yolo12s.pt",
+    "yolov12m": "yolo_output/yolo_models/yolo12m.pt",
+    "yolov12l": "yolo_output/yolo_models/yolo12l.pt",
+    "yolov12x": "yolo_output/yolo_models/yolo",
 }
