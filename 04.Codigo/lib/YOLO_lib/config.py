b3923ecd23f4f77131582b694028c7691877d3ef
Aitor <agarcb20@estudiantes.unileon.es>
2025-07-21 00:03:47 +0200

    kfold + kfold_utils

--- 04.Codigo/lib/YOLO_lib/config.py
+++ 04.Codigo/lib/YOLO_lib/config.py
@@ -1,11 +1,15 @@
 SEED = 42
-N_TRIALS = 1
-EPOCH_OPTUNA = 2
-EPOCH_TRAIN = 2
+N_TRIALS = 3
+EPOCH_OPTUNA = 25
+EPOCH_TRAIN = 50
 BATCH = 15
 IMGSZ = 704
+K = 5
 
 models = {
+    # YOLOv10
+    "yolov10n": "yolo_output/yolo_models/yolov10n.pt",
+
     # YOLOv11
     "yolov11n": "yolo_output/yolo_models/yolo11n.pt",
     "yolov11s": "yolo_output/yolo_models/yolo11s.pt",
@@ -23,6 +27,7 @@ models = {
 }
 
 results_csv_paths = {
+    'yolov10n_optuna': 'runs/detect/final_model_yolov10n/results.csv',
     'yolov12n': 'runs/detect/yolov12n/results.csv',
     'yolov11n': 'runs/detect/yolov11n/results.csv',
     'yolov11l': 'runs/detect/yolov11l/results.csv',
