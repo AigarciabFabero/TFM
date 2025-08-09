ca9a8219b6d0fd18e27af43a109d5a1f407c3276
Aitor <agarcb20@estudiantes.unileon.es>
2025-08-09 17:24:04 +0200

    Cambios en la asignación de parámetros

--- 04.Codigo/lib/YOLO_lib/config.py
+++ 04.Codigo/lib/YOLO_lib/config.py
@@ -36,3 +36,94 @@ results_csv_paths = {
     'yolov11s': 'runs/detect/final_model_yolov11s/results.csv',
     'yolov12s': 'runs/detect/final_model_yolov12s/results.csv',
 }
+
+# v8
+best_params_yolov8s = {
+    "lr0": 0.005399484409787433,
+    "lrf": 0.003991305878561679,
+    "momentum": 0.9062108866694067,
+    "weight_decay": 0.00010485387725194633,
+    "optimizer": "SGD",
+    "warmup_epochs": 5,
+    "warmup_momentum": 0.75,
+    "degrees": 45,
+    "translate": 0.1,
+    "scale": 0.06,
+    "flipud": 0.5,
+    "fliplr": 0.5,
+    "mosaic": 0,
+    "close_mosaic": 0,
+}
+
+#v9
+best_params_yolov9s = {
+    "lr0": 0.0002310201887845295,
+    "lrf": 0.0015254729458052604,
+    "momentum": 0.8456363364439307,
+    "weight_decay": 0.0003347776308515934,
+    "optimizer": "AdamW",
+    "warmup_epochs": 5,
+    "warmup_momentum": 0.75,
+    "degrees": 45,
+    "translate": 0.1,
+    "scale": 0.06,
+    "flipud": 0.5,
+    "fliplr": 0.5,
+    "mosaic": 0,
+    "close_mosaic": 0,
+}
+
+#v10
+best_params_yolov10s = {
+    "lr0": 0.0005611516415334506,
+    "lrf": 0.00892718030435363,
+    "momentum": 0.9097990912717108,
+    "weight_decay": 0.00039687933304443713,
+    "optimizer": "SGD",
+    "warmup_epochs": 5,
+    "warmup_momentum": 0.75,
+    "degrees": 45,
+    "translate": 0.1,
+    "scale": 0.06,
+    "flipud": 0.5,
+    "fliplr": 0.5,
+    "mosaic": 0,
+    "close_mosaic": 0,
+}
+
+#v11
+best_params_yolov11s = {
+    "lr0": 0.0002310201887845295,
+    "lrf": 0.0015254729458052604,
+    "momentum": 0.8456363364439307,
+    "weight_decay": 0.0003347776308515934,
+    "optimizer": "AdamW",
+    "warmup_epochs": 5,
+    "warmup_momentum": 0.75,
+    "degrees": 45,
+    "translate": 0.1,
+    "scale": 0.06,
+    "flipud": 0.5,
+    "fliplr": 0.5,
+    "mosaic": 0,
+    "close_mosaic": 0,
+}
+
+
+#v12
+best_params_yolov12s = {
+    "lr0": 0.00023426581058204064,
+    "lrf": 0.009323621351781481,
+    "momentum": 0.9162699235041671,
+    "weight_decay": 0.0008699593128513321,
+    "optimizer": "AdamW",
+    "warmup_epochs": 5,
+    "warmup_momentum": 0.75,
+    "degrees": 45,
+    "translate": 0.1,
+    "scale": 0.06,
+    "flipud": 0.5,
+    "fliplr": 0.5,
+    "mosaic": 0,
+    "close_mosaic": 0,
+}
